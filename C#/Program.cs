26dbfcf52a3f7e34fa63145b39d30b990f014e3e
Yanosch <yan_osch@gmx.de>
2021-06-27 16:41:04 +0200

    Add a line that clears the text

--- C#/Program.cs
+++ C#/Program.cs
@@ -28,11 +28,13 @@ namespace HuffmanTree
             time = DateTime.Now - start;
             Console.WriteLine("Message encoded. took {0}h {1}min {2}s {3}ms", time.Hours, time.Minutes, time.Seconds, time.Milliseconds);
             
-            tree.text = new byte[0];
+            tree.text = new byte[1];
+            Console.WriteLine("Clear the text: {0}", Tools.ByteToString(tree.text));
             start = DateTime.Now;
             tree.Decode();
             time = DateTime.Now - start;
             Console.WriteLine("Message decoded. took {0}h {1}min {2}s {3}ms", time.Hours, time.Minutes, time.Seconds, time.Milliseconds);
+            Console.WriteLine("uncompressed text: {0} Byte\ncompressed text {1} Byte",tree.text.Length, tree.encodedText.Length);
 
             Console.Write("Do you want to see the decoded text [y/n]");
             string input = Console.ReadLine();
